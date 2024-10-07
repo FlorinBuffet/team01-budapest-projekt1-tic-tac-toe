843f1eed3d6e68df7422978f3f1e0e5cfa5606a1
Midas Pavia Dominguez <paviamid@students.zhaw.ch>
2024-10-07 12:15:16 +0200

    User interface test class with // marked, indicating it doesn’t compile

--- src/UserinterfaceTest.java
+++ src/UserinterfaceTest.java
@@ -1,7 +1,7 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-public class UserinterfaceTest {
+//public class UserinterfaceTest {
 
     //@Test
     //public void testChangeLanguage() {
@@ -11,5 +11,5 @@ public class UserinterfaceTest {
        // ui.changeLanguage("English");
        // assertEquals("English", ui.getLanguage());
 
-    }
-}
+//    }
+//}
