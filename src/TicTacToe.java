d316d03015dfbda414db17b9d15499d52d64ab11
Leo Widmer <widmele2@students.zhaw.ch>
2024-10-15 14:17:40 +0200

    add game loop

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -2,16 +2,19 @@
  * Initialises and starts the Tic Tac Toe game
  *
  * @author Widmer Leo
- * @version 1.0
+ * @version 1.1
  */
 public class TicTacToe {
     /**
-     * Creates Board, Game and UserInterface objects and starts a new game
+     * Creates Board, Game and UserInterface objects and starts the game loop
      */
     public static void main(String[] args) {
         Board board = new Board();
         Game game = new Game(board);
         UserInterface ui = new UserInterface(game,board);
-        ui.newGame();
+        do {
+            ui.newGame();
+        }while(game.getNextGame());
+        
     }
 }
